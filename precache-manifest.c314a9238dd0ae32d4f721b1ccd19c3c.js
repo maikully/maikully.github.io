75dbcbb4240d045cf8e0e602aa4948705da1dada
maikully <michael_s_li@brown.edu>
2020-06-07 17:50:29 -0400

    Updates

---
+++ precache-manifest.c314a9238dd0ae32d4f721b1ccd19c3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8129f8d4780da07c70e05f0d2f76f58a",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "8a2eb2b9b93fec78e126",
+    "url": "/react-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "026d6c3d7833c89d4a2a",
+    "url": "/react-site/static/js/2.ae700e3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-site/static/js/2.ae700e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2eb2b9b93fec78e126",
+    "url": "/react-site/static/js/main.07efe83a.chunk.js"
+  },
+  {
+    "revision": "31c7e709a99abdf0e13c",
+    "url": "/react-site/static/js/runtime-main.51f103d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
